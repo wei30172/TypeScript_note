54ae5a2a060f6f4415f60f86a336c1b90ff72212
wei30172 <60259324+wei30172@users.noreply.github.com>
2020-12-31 11:33:03 +0800

    Lesson 5

---
+++ TS-5.js
@@ -0,0 +1,33 @@
+// [explicit types 明確型別宣告]
+var food;
+var price;
+var isHotProduct;
+// price = 'noodle';
+price = 15;
+// food = 10;
+food = 'noodle';
+// isHotProduct = 100;
+isHotProduct = true;
+// arrays
+// let meal: string[];
+var meal = []; // 宣告為空陣列，才可操作陣列
+// meal = [99, 150, 200];
+meal.push('bread');
+meal.push('noodle');
+console.log(meal);
+// [union types 複合型別 or 聯合型別]
+var mixed = [];
+mixed.push('yoyoyo');
+mixed.push(false);
+mixed.push(99);
+console.log(mixed);
+var uid;
+// uid = true;
+uid = '123';
+uid = 123;
+console.log(uid);
+// objects
+var setOne;
+setOne = { type: 'Italian', price: 500 };
+var setTwo;
+setTwo = { type: 'American', price: 250, description: 'popular' };
---
+++ TS-5.ts
@@ -0,0 +1,48 @@
+// [explicit types 明確型別宣告]
+
+let food: string;
+let price: number;
+let isHotProduct: boolean;
+
+// price = 'noodle';
+price = 15;
+// food = 10;
+food = 'noodle';
+
+// isHotProduct = 100;
+isHotProduct = true;
+
+// arrays
+// let meal: string[];
+let meal: string[] = [];  // 宣告為空陣列，才可操作陣列
+
+// meal = [99, 150, 200];
+
+meal.push('bread');
+meal.push('noodle');
+console.log(meal);
+
+// [union types 複合型別 or 聯合型別]
+let mixed: (string|number|boolean)[] = [];
+mixed.push('yoyoyo');
+mixed.push(false);
+mixed.push(99);
+console.log(mixed);
+
+let uid: string|number;
+// uid = true;
+uid = '123';
+uid = 123;
+
+console.log(uid);
+
+// objects
+let setOne: object;
+setOne = { type: 'Italian', price: 500 };
+
+let setTwo: {
+  type: string,
+  price: number,
+  description: string
+};
+setTwo = { type: 'American', price: 250, description: 'popular' };
