603570f32e02af681eacf69d43c9fe7e4dd8d8b9
wei30172 <60259324+wei30172@users.noreply.github.com>
2020-12-31 11:29:58 +0800

    Lesson 2

---
+++ TS-2.js
@@ -0,0 +1,7 @@
+var food = 'rice';
+console.log(food);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ TS-2.ts
@@ -0,0 +1,11 @@
+const food = 'rice';
+
+console.log(food);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+});
